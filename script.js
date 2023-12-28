314cdb66eae9e68c6ead8117f23563ee43872313
Faroukdev001 <faroukarogundade@gmail.com>
2023-12-28 23:34:35 +0100

    improved the logic of the game in the script.js file

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Hello Farouk");
+function getPlayerChoice() {
+    return prompt("Enter a choice").toLowerCase(); 
+}
+
+
+function getComputerChoice() {
+    const moves = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * moves.length);
+    return moves[randomIndex]; 
+}
+
+    
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "It's a tie";
+    }
+    else if (
+        playerChoice === "rock" && computerChoice === "scissors" ||
+        playerChoice === "paper" && computerChoice === "rock" ||
+        playerChoice === "scissors" && computerChoice === "paper"
+    ) {
+        return "You win"; 
+    }
+    else {
+        return "Computer Wins";
+    }
+}
+
+// Main game logic
+const playerChoice = getPlayerChoice();
+const computerChoice = getComputerChoice();
+console.log(playRound(playerChoice, computerChoice));
